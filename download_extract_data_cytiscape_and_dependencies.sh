93e039020a3c0dd6e1d3d84d9e9311c8d28e710f
dcastf01 <59411917+dcastf01@users.noreply.github.com>
2020-09-14 12:08:02 +0200

    adding bash to insert dataset

---
+++ download_extract_data_cytiscape_and_dependencies.sh
@@ -0,0 +1,15 @@
+
+wget --keep-session-cookies --save-cookies=cookies.txt --post-data 'username=dacasfal@upv.es&password=testeando02&submit=Login' https://www.cityscapes-dataset.com/login/
+wget --load-cookies cookies.txt --content-disposition https://www.cityscapes-dataset.com/file-handling/?packageID=1 #imagen etiquetada
+wget --load-cookies cookies.txt --content-disposition https://www.cityscapes-dataset.com/file-handling/?packageID=3 #imagenes reales 
+mkdir dataset
+mv /content/leftImg8bit_trainvaltest.zip dataset
+mv /content/gtFine_trainvaltest.zip dataset
+mkdir dataset/cityscapes
+unzip dataset/leftImg8bit_trainvaltest.zip -d /content/dataset/cityscapes
+unzip -o dataset/gtFine_trainvaltest.zip -d /content/dataset/cityscapes
+git clone https://github.com/dcastf01/cityscapesScripts.git
+pip install /content/cityscapesScripts
+pip install cityscapesscripts[gui]
+export CITYSCAPES_DATASET=/content/dataset/cityscapes
+python /content/cityscapesScripts/cityscapesscripts/preparation/createTrainIdLabelImgs.py
