48f36c070cc4a31034106168bac7b4c1def87f84
dcastf01 <dcastf01@gmail.com>
2020-09-15 07:12:20 +0000

    add imports

---
+++ utils_visualization.py
@@ -0,0 +1,17 @@
+
+def display(display_list):
+  plt.figure(figsize=(15, 15))
+
+  title = ['Input Image', 'True Mask', 'Predicted Mask']
+
+  for i in range(len(display_list)):
+    plt.subplot(1, len(display_list), i+1)
+    plt.title(title[i])
+    
+    if np.amax(display_list[i])<=1:
+      plt.imshow(tf.keras.preprocessing.image.array_to_img(display_list[i],scale=True))
+    else:
+      plt.imshow(tf.keras.preprocessing.image.array_to_img(display_list[i],scale=False),vmin=0,vmax=NUM_CLASSES-1)
+    
+    plt.axis('off')
+  plt.show()
