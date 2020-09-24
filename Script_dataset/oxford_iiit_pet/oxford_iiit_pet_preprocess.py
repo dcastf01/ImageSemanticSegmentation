8907e8a496dbfb3376aa8dd094682963043ef1a5
David Castellano <david@beiota.com>
2020-09-24 09:20:52 +0200

    Update oxford_iiit_pet_preprocess.py

--- Script_dataset/oxford_iiit_pet/oxford_iiit_pet_preprocess.py
+++ Script_dataset/oxford_iiit_pet/oxford_iiit_pet_preprocess.py
@@ -1,7 +1,7 @@
 import tensorflow as tf
 
 class create_dataset():
-    def __init__(self,dataset_train,dataset_test,dataset_validation,
+    def __init__(self,dataset_train,dataset_test,dataset_validation,Which_dataset,
                 IMAGE_SIZE:tuple,PERCENT_INCREMENTED_IN_JITTER:float,PROBABLITY_THRESHOLD:float):
         
         self.train=dataset_train.map(self.load_train_image, num_parallel_calls=tf.data.experimental.AUTOTUNE)
@@ -10,7 +10,7 @@ class create_dataset():
         self.IMAGE_SIZE=IMAGE_SIZE
         self.PERCENT_INCREMENTED_IN_JITTER=PERCENT_INCREMENTED_IN_JITTER
         self.PROBABLITY_THRESHOLD=PROBABLITY_THRESHOLD
-
+        self.Which_dataset=Which_dataset
 
     def resize(self,inimg,tgimg,heigth,width):
 
@@ -22,7 +22,7 @@ class create_dataset():
     def normalize(self,inimg,tgimg):
         
         inimg=(inimg/255)
-        if Which_dataset=='oxford_iiit_pet':
+        if self.Which_dataset=='oxford_iiit_pet':
             tgimg -= 1  
 
         return inimg,tgimg
@@ -39,7 +39,7 @@ class create_dataset():
         original_shape = tf.shape(inimg)[-3:-1]
         num_image_channels = tf.shape(inimg)[-1]
 
-        inimg,tgimg=resize(inimg,tgimg,heigth_incremented,width_incremented)
+        inimg,tgimg=self.resize(inimg,tgimg,heigth_incremented,width_incremented)
         stacked_image = tf.concat([inimg, tf.cast(tgimg, dtype=inimg.dtype)], axis=-1)
         num_stacked_channels = tf.shape(stacked_image)[-1]
 
@@ -70,16 +70,16 @@ class create_dataset():
         input_image=datapoint['image']
         target_mask=datapoint['segmentation_mask']
             
-        input_image,target_mask = resize(input_image,target_mask,self.IMAGE_SIZE[0],self.IMAGE_SIZE[1])
+        input_image,target_mask = self.resize(input_image,target_mask,self.IMAGE_SIZE[0],self.IMAGE_SIZE[1])
         
         if augment:
-            input_image,target_mask=random_jitter(input_image,target_mask,self.IMAGE_SIZE[0],self.IMAGE_SIZE[1])
+            input_image,target_mask=self.random_jitter(input_image,target_mask,self.IMAGE_SIZE[0],self.IMAGE_SIZE[1])
             
-        input_image, target_mask = normalize(input_image, target_mask)
+        input_image, target_mask = self.normalize(input_image, target_mask)
 
         return input_image, target_mask
 
     def load_train_image(self,datapoint):
-        return load_image(datapoint,True)
+        return self.load_image(datapoint,True)
     def load_test_image(self,datapoint):
-        return load_image(datapoint,False)
+        return self.load_image(datapoint,False)
